87ab574021bbcd64ff62b76a425b2f5b90ab0c17
Manocchio <guruu.leste@gmail.com>
2021-12-06 00:01:53 -0300

    update cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 var emailsRouter = require('./routes/mailer');
 var app = express();
@@ -10,7 +11,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
+
 app.use('/', emailsRouter);
-app.listen(86);
+app.listen(process.env.PORT || 86);
 
 module.exports = app;
